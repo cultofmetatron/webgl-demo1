28a455a45d010604cbf9a59442b21a2075226dc6
Peter de Croos <cultofmetatron@aumlogic.com>
2014-01-21 14:03:46 -0800

    added scripts and stylus directories

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,22 @@ module.exports = function(grunt) {
 
   var conf = {};
   conf.pkg = grunt.file.readJSON('package.json');
-  
+  conf.browserify = {
+    files: {
+      'application.js': [
+        
+      ]
+    },
+    options : {}
+  };
+
+
   grunt.initConfig(configs);
 
+  
 
+
+  grunt.loadNpmTasks('grunt-browserify');
 
 
 
