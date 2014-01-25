61532ec94fd5d7ad74504eeefd09238afb4e1f98
Peter de Croos <cultofmetatron@aumlogic.com>
2014-01-25 00:54:47 -0800

    loading up the dot

--- assets/scripts/main/main.js
+++ assets/scripts/main/main.js
@@ -1,26 +1,27 @@
 var glTools = require('../shared/gltools.js');
 
-var gl = glTools.initWebGL($('canvas#canvasable')[0]);
-
-gl.clearColor(0.5, 0.8, 0.2, 1.0);
-gl.clear(gl.COLOR_BUFFER_BIT);
-
-var fs = require('fs');
-
 glTools.getShaders('vshader', 'fshader')
   .spread(function(vshaderS, fshaderS) {
-    console.log(vshaderS);
-    console.log(fshaderS);
-  });
+    return {
+      vshader: vshaderS,
+      fshader: fshaderS
+    };
+  }).then(draw);
 
 
-/*
-($.ajax(
-    '/shaders/' + 'vshader' + '.js',
-    {
-    method: 'GET',
-    dataType: 'text',
-  })).done(function() { 
-    console.log(arguments);
-  });
-*/
+function draw(options) {
+  var gl = glTools.initWebGL($('canvas#canvasable')[0]);
+  if (!glTools.initShaders(gl, options.vshader, options.fshader)) {
+    console.error('failed to iitialize shader');
+    return null;
+  }
+  
+  gl.clearColor(0.5, 0.8, 0.2, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  //draw a point
+  gl.drawArrays(gl.POINTS, 0, 1);
+  //gl.drawArrays(gl.POINTS, 1, 0);
+
+}
+
--- assets/scripts/shared/gltools.js
+++ assets/scripts/shared/gltools.js
@@ -36,7 +36,67 @@ var getShaders = function() {
 module.exports.getShader  = getShader;
 module.exports.getShaders = getShaders;
 
+var loadShader = function(gl, type, source) {
+  var shader = gl.createShader(type);
+  if (shader === null) {
+    console.error('unable to create shader');
+    return null;
+  }
+  //set the shader program
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
 
+  var compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (!compiled) {
+    var error = gl.getShaderInfoLog(shader);
+    console.error('failed to compile shader :' + error);
+    gl.deleteShader(shader);
+    return null;
+  }
+  return shader;
+};
 
+var createProgram = function(gl, vshader, fshader) {
+  //create the shader object
+  var vertexShader = loadShader(gl, gl.VERTEX_SHADER, vshader);
+  var fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fshader);
+  if (!vertexShader || !fragmentShader) { return null; }
+
+  //create a program object
+  var program = gl.createProgram();
+  if (!program) {
+    return null;
+  }
+  
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+
+  //link the program object to gl
+  gl.linkProgram(program);
+
+  //verify linking
+  var linked = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (!linked) {
+    var error = gl.getProgramInfoLog(program);
+    console.error('FAiled to link program' + error);
+    gl.deleteProgram(program);
+    gl.deleteShader(fragmentShader);
+    gl.deleteShader(vertexShader);
+    return null;
+  }
+  return program;
+};
+
+var initShaders = function(gl, vshader, fshader) {
+  var program = createProgram(gl, vshader, fshader);
+  if (!program) {
+    console.log('failed to create the program');
+    return false;
+  }
+  gl.useProgram(program);
+  gl.program = program;
+  return true;
+};
+module.exports.initShaders = initShaders;
 
 
